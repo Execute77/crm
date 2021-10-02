bfc7e110f5b061d8a95c6b3f94c0ae6e8f176d21
Execute77 <byahattivijay@gmail.com>
2021-10-02 15:38:19 +0530

    gvhbjn

--- src/test/java/com/testcases/BaseTest.java
+++ src/test/java/com/testcases/BaseTest.java
@@ -1,52 +1,30 @@
 package com.testcases;
 
 import com.utilities.ConfigFactory;
-import com.utilities.ConfigProvider;
 import com.utilities.DriverFactory;
-import com.utilities.TestDataProvider;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
     static WebDriver driver;
     static ConfigFactory config;
-    static XSSFWorkbook testData;
 
     @BeforeSuite
     public void initialSetup() {
         config = new ConfigFactory();
-//        driver = DriverFactory.getDriver(config.getBrowser());
-    }
-
-    @BeforeMethod
-    public void setUp() {
-        System.out.println("Before test");
-//        driver = DriverFactory.getDriver(config.getBrowser(), config.getURL());
-//        driver = DriverFactory.getDriver(config.getBrowser(), config.getURL());
         driver = DriverFactory.getDriver(config.getBrowser());
-
     }
-
-//    @AfterTest
-//    public void tearDown() {
-//        DriverFactory.quitBrowser(driver);
-//    }
-
 
 //    @BeforeMethod
-//    public void beforeMethod(){
-//        System.out.println("Before method driver is "+driver);
-//    }
+//    public void setUp() {
+//        System.out.println("Before Method");;
+//        driver = DriverFactory.getDriver(config.getBrowser());
 //
-    @AfterMethod
-    public void afterMethod(){
-        System.out.println("After method driver is - "+driver);
+//    }
+
+    @AfterTest
+    public void tearDown() {
         DriverFactory.quitBrowser(driver);
     }
 }
